683f6008e9fe0ebcd3cc52f50750b0b2fb44b753
Valentin Gonzalez <VALENTIN@iMac-de-RODRIGO.local>
2019-06-29 17:59:09 -0500

    search overlay added

---
+++ js.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('load', evt => {
+    const searchBar = document.getElementById('searchBar');
+    const closeSearchBt = document.getElementById('closeSearch');
+
+    searchBar.addEventListener('focus', () => {document.body.classList.add('searching')});
+    closeSearchBt.addEventListener('click', () => document.body.classList.remove('searching'));
+})
